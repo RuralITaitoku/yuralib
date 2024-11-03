be8151a65c0b8665d8182af4ebc5df366ec8c78e
Yoshio Hisadomi <yururelaxbody@gmail.com>
2024-11-03 15:30:37 +0900

    2024/11/03

--- yuralib.sh
+++ yuralib.sh
@@ -1,7 +1,13 @@
 
 set -x
-
+export YURA_HOME=/mnt/c/Users/yurur/relax/yuralib
 if [ "$1" = "add" ]; then
-	echo tttttttttttttttttttttttttttttt
-
+	echo $YURA_HOME
+	mkdir -p $YURA_HOME/src
+	cd $YURA_HOME/src
+	if [ "$2" = "ruralis2" ]; then
+		echo test
+		pwd
+		ls
+	fi
 fi
