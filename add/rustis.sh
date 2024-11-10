d161d6f4980150cf80f00ea777dfc63df61c6205
Yoshio Hisadomi <yururelaxbody@gmail.com>
2024-11-10 17:48:52 +0900

    2024/11/10

--- add/rustis.sh
+++ add/rustis.sh
@@ -1,17 +1,20 @@
 
+export PRJ=rustis
+export PRJ_REP=git@github.com:RuralITaitoku/rustis.git
 set +x
 mkdir -p $YURA_HOME/prj
 cd $YURA_HOME/prj
-git clone git@github.com:RuralITaitoku/rustis.git
+git clone $PRJ_REP
 
-cd $YURA_HOME/prj/rustis
-
+cd $YURA_HOME/prj/$PRJ
 
 if which cargo ; then
     cargo build
 else
     echo carugoなし
 fi
+
+
 
 
 
