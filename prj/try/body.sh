2aa0bc35d9cf8d5b7dd1cbba348eb05ea92416f9
Yoshio Hisadomi <yururelaxbody@gmail.com>
2024-12-31 22:06:06 +0900

    2024/12/31

--- prj/try/body.sh
+++ prj/try/body.sh
@@ -7,6 +7,8 @@ if [ "$1" = "run" ]; then
 elif [ "$1" = "cap" ]; then
 	cd ./build/try-cv-241215
 	./try-cv
+elif [ "$1" = "clean" ]; then
+	rm -r build
 else
 	cmake -S src -B build
 	cmake --build build
