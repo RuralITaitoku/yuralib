74e9e0c3e482c9c6fc32b3e915c35b989b04da93
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-04-26 00:17:20 +0900

    2025/04/25

--- prj/ivvi/include/yura25.hpp
+++ prj/ivvi/include/yura25.hpp
@@ -22,12 +22,14 @@ namespace yura {
      */
     std::vector<std::string> split(const std::string& str, const std::string& term = "\n");
 
+
+    // 次のコメントのメソッドをC++14で実装して。
     /**
      * @brief 文字列の開始位置から指定UTF9の文字数までのバイト数を求める。
      * @param str 対象の文字列
      * @param start　開始バイト位置
      * @param utf8_size UTF8の文字数
-     * @return 分割した文字列の配列
+     * @return バイト数
      */
     int get_utf8_byte_size(const std::string& str, int start_byte, int utf8_size = 1);
 
