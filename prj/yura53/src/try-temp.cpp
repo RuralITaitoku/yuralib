0d9338b12313f418616a603d28670ef97c7d1795
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-05-21 05:18:48 +0900

    2025/05/21

---
+++ prj/yura53/src/try-temp.cpp
@@ -0,0 +1,16 @@
+
+
+class EucCost {
+public:
+    virtual const double position() const = 0;
+};
+class IoU {
+public:
+    virtual const double position() const = 0;
+};
+
+class Track {
+public:
+
+};
+
