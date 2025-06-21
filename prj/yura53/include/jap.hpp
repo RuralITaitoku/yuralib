c3a6b31018165d7ae7f876e7b49c569a647bfc0b
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-06-21 21:57:05 +0900

    2025/06/21

--- prj/yura53/include/jap.hpp
+++ prj/yura53/include/jap.hpp
@@ -5,5 +5,5 @@ class jap {
 
     virtual std::string name() = 0;
     virtual std::string help() = 0;
-    virtual int loop(std::vector<std::string>& stack) == 0;
-}
+    virtual int loop(std::vector<std::string>& stack) = 0;
+};
