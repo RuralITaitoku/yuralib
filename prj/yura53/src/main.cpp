ba5fbba76cb232a75604f70fb2a3e2e1cb427e39
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-07-01 05:41:26 +0900

    2025/07/01

--- prj/yura53/src/main.cpp
+++ prj/yura53/src/main.cpp
@@ -117,6 +117,7 @@ int main(int argc, char** argv) {
     std::string input_filename;
     std::string output_filename;
     std::string template_filename;
+    std::string replace_word = "@@@html@@@";
     for (size_t i = 0; i < (args.size() - 1); i++) {
         auto arg = args[i];
         if (arg == "-i") {
@@ -125,6 +126,8 @@ int main(int argc, char** argv) {
             output_filename = args[++i];
         } else if (arg == "-t") {
             template_filename = args[++i];
+        } else if (arg == "-rw") {
+            replace_word = args[++i];
         }
     }
     // DP("input : " << input_filename);
@@ -146,7 +149,7 @@ int main(int argc, char** argv) {
             auto str_html = md.html(str_md);
             auto str_template = yura::load(template_filename);
             //std::cout << str_template << std::endl;
-            yura::save(output_filename, yura::replace_all(str_template, "@@@html@@@", str_html));
+            yura::save(output_filename, yura::replace_all(str_template, replace_word, str_html));
         }
         if (arg == "jap") {
             jap_notation();
