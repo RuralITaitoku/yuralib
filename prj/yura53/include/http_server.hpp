e41febc56df9b1904791c2db29af6c2a8034fae2
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-09-20 07:54:35 +0900

    20250920

---
+++ prj/yura53/include/http_server.hpp
@@ -0,0 +1,8 @@
+
+
+class http_server {
+    public:
+        http_server(int port_no);
+
+        
+}
--- prj/yura53/src_jap/jap_zero.cpp
+++ prj/yura53/src_jap/jap_zero.cpp
@@ -1,5 +1,5 @@
 #include "jap_zero.hpp"
-#include "yura.hpp"
+#include <cstdlib>
 
 template<typename T> class TypeCheck;
 #define DP(x) std::cout << __FILE__ << ":" << __LINE__ << " " << x << std::endl;
@@ -10,20 +10,50 @@ bool jap_zero::run(std::string &cmd, std::vector<std::string> &stack) {
     return false;
 }
 void jap_zero::main() {
-    std::cout << "jap0 main" << std::endl;
     std::string line;
     std::vector<std::string> stack;
     for(;;) {
         getline(std::cin, line);
-        std::cout << line << std::endl;
-
+        if (line == ".") {
+            if (stack.size()) {
+                std::cout << stack.back() << std::endl;
+                stack.pop_back();
+            }
+        } else if (line == "..") {
+            std::cout << "-stack-" << stack.size() << std::endl;
+            if (stack.size()) {
+                for (size_t i = 0; i < stack.size(); i++) {
+                    std::cout << i << ":" << stack[i] << std::endl;
+                }
+            }
+            std::cout << "-" << std::endl;
+        } else if (line == "clear") {
+            stack.clear();
+        } else if (line == "drop") {
+            if (stack.size()) {
+                stack.pop_back();
+            }
+        } else if (line == "s" || line == "status") {
+            auto result = std::system("git status");
+            if (result == 0) {
+                std::cout << "正常終了" << std::endl;
+            } else {
+                std::cout << "異常終了:" << result << std::endl;
+            }
+        } else if (line == "commit") {
+        } else if (line == "q") {
+                std::cout << "jap終了" << std::endl;
+            return;
+        } else {
+            stack.push_back(line);
+        }
     }
 
 }
 
 
 int main() {
-    std::cout << "main jap" << std::endl;
+    std::cout << "jap" << std::endl;
     jap_zero jap0;
 
     jap0.main();
