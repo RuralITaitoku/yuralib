141f5aed762df4c57c50e5f3bdd881857afcd3d2
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-10-21 05:15:38 +0900

    20251021

--- prj/yura53/src/yura.cpp
+++ prj/yura53/src/yura.cpp
@@ -122,6 +122,24 @@ int yura::get_utf8_byte_size(const std::string& str, size_t start_byte, int utf8
     }
     return current_byte - start_byte;
 }
+int yura::get_char_bytes(unsigned char first_byte) {
+    int char_bytes;
+    if ((first_byte & 0x80) == 0) { // 0xxxxxxx (1 byte)
+        char_bytes = 1;
+    } else if ((first_byte & 0xE0) == 0xC0) { // 110xxxxx (2 bytes)
+        char_bytes = 2;
+    } else if ((first_byte & 0xF0) == 0xE0) { // 1110xxxx (3 bytes)
+        char_bytes = 3;
+    } else if ((first_byte & 0xF8) == 0xF0) { // 11110xxx (4 bytes)
+        char_bytes = 4;
+    } else if ((first_byte & 0xFC) == 0xF8) { // 111110xx (5 bytes)
+        char_bytes = 5;
+    } else {
+        // 不正なUTF-8シーケンス
+        throw std::invalid_argument("不正なUTF-8シーケンスが文字列に含まれています。");
+    }
+    return char_bytes;
+}
 
 // 左トリム (Leading whitespace)
 std::string yura::ltrim(const std::string& s) {
@@ -385,8 +403,6 @@ int64_t yura::diff_milli_secound(uint64_t start, uint64_t end) {
     return diff_seconds * 1000 + diff_ms;
 }
 
-
-
 // e29480	─	━	│	┃	┄	┅	┆	┇	┈	┉	┊	┋	┌	┍	┎	┏
 // e29490	┐	┑	┒	┓	└	┕	┖	┗	┘	┙	┚	┛	├	┝	┞	┟
 // e294a0	┠	┡	┢	┣	┤	┥	┦	┧	┨	┩	┪	┫	┬	┭	┮	┯
@@ -399,8 +415,6 @@ int64_t yura::diff_milli_secound(uint64_t start, uint64_t end) {
 // e29690	▐	░	▒	▓	▔	▕	▖	▗	▘	▙	▚	▛	▜	▝	▞	▟
 // e296a0	■	□	▢	▣	▤	▥	▦	▧	▨	▩	▪	▫	▬	▭	▮	▯
     // "▖" "▗" "▘" "▝" "▙" "▚" "▛" "▜" "▞" "▟" "▐" "▎"
-
-
 int yura::exec(std::string& cmd) {
     yura_pipe pipe;
     pipe.exec(cmd);
