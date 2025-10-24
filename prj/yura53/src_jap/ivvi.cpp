7b05eb8de87fcd7d9fa6cbc88f0878d329f12f9a
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-10-25 07:46:47 +0900

    20251025

--- prj/yura53/src_jap/ivvi.cpp
+++ prj/yura53/src_jap/ivvi.cpp
@@ -29,17 +29,21 @@ bool ivvi::run(std::string &cmd, std::vector<std::string>& stack) {
         } else if (ch == 'j') {
             if (rrow() < height) {
                 row = rrow() + 1;
+                col = rcol();
             }
         } else if (ch == 'k') {
             if (rrow() > 1) {
                 row = rrow() - 1;
+                col = rcol();
             }
         } else if (ch == 'h') {
             if (rcol() > 1) {
+                row = rrow();
                 col = rcol() - 1;
             }
         } else if (ch == 'l') {
             if (rcol() < width) {
+                row = rrow();
                 col = rcol() + 1;
             }
         }
