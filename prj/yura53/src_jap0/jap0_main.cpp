f453193db345aa68aa7efe5218e128e2a767569f
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-12-14 09:00:37 +0900

    20251214

--- prj/yura53/src_jap0/jap0_main.cpp
+++ prj/yura53/src_jap0/jap0_main.cpp
@@ -2,9 +2,32 @@
 
 #include "jap0.hpp"
 #include "yuraterm.hpp"
+#include <regex>
+
+bool check_line(const std::string& text, std::string& prefix, int& no, std::string& line) {
+    std::string src;
+    if ((text.size() > 0) && text[0] == 'a') {
+        prefix = "a";
+        src = text.substr(1);
+    } else {
+        src = text;
+    }
+    std::regex line_pattern(R"(([0-9]*)\. (.*))");
+    std::smatch matches;
+    if (std::regex_search(src, matches, line_pattern)) {
+        no = std::stoi(matches[1].str());
+        line = matches[2].str();
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main(int argc, char** argv) {
     std::string cmd;
+    int row_no;
+    std::string row_cmd;
+    std::string row_line;
 
     std::vector<std::string> stack;
     std::vector< std::shared_ptr<jap> > japs;
@@ -24,6 +47,8 @@ int main(int argc, char** argv) {
             for (auto jap : japs) {
                 std::cout << jap->help() << std::endl;
             }
+        } else if (check_line(cmd, row_cmd, row_no, row_line)) {
+            std::cout << row_cmd << " " << row_no << ". " << row_line << std::endl;
         } else {
             bool run_flag = false;
             for (auto jap : japs) {
