9a81f6e713306ef17f729b429adb88d1209c5609
Yoshio Hisadomi <yururelaxbody@gmail.com>
2025-12-25 05:40:17 +0900

    20251225

--- prj/yura53/src_jap0/jap0.cpp
+++ prj/yura53/src_jap0/jap0.cpp
@@ -53,8 +53,6 @@ bool jap0::run(const std::string& cmd, std::vector<std::string>& stack) {
     return false;
 }
 
-
-
 jap0_term::jap0_term() {
     std::cout << "jap0_term" << std::endl;
     struct termios new_termios;
