c12507806374a2276fdd4084b5f7c40d7040e9e3
Yoshio Hisadomi <yururelaxbody@gmail.com>
2026-05-10 05:08:54 +0900

    20260510

--- prj/yura54/src_jap54/jap54.cpp
+++ prj/yura54/src_jap54/jap54.cpp
@@ -132,6 +132,33 @@ TEST(TestJap54, get_char_bytes_ut_01) {
 #endif
 
 int jap54::get_utf8_term_width(const std::string& str, size_t start_byte, int utf8_size) {
-    return 0;
+    if (start_byte < 0 || start_byte >= str.length()) {
+        throw std::out_of_range("The startting byte postion is outside the range of the string.");
+    }
+    if (utf8_size <= 0) {
+        throw std::invalid_argument("The number of characters in a UTF-8 encoding must be a positive number.");
+    }
+
+    size_t current_byte = start_byte;
+    int processed_chars = 0;
+    int term_width = 0;
+    while (processed_chars < utf8_size && current_byte < str.length()) {
+        unsigned char first_byte = static_cast<unsigned char>(str[current_byte]);
+        int char_bytes = get_char_bytes(first_byte);
+
+        if (current_byte + char_bytes > str.length()) {
+            // 文字列の終わりを超えた
+            throw std::out_of_range("UTF8 characters is outside the range of the string.");
+        }
+
+        current_byte += char_bytes;
+        processed_chars++;
+        if (char_bytes == 1) {
+            term_width += 1;
+        } else {
+            term_width += 2;
+        }
+    }
+    return current_byte - start_byte;
 }
 
